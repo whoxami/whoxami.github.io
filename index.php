32de24baeb92c4b7a6bce5552169421aac631639
whoxami <122668748+whoxami@users.noreply.github.com>
2023-09-24 02:09:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<html>
+<head>
+<title>空の花</title>
+</head>
+<body bgcolor="#19181c">
+<div class="bg">
+<div class="menu">
+<br>
+&nbsp;
+<span class="p1">空の花</span>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="index.php"><button class="btn">Ana Sayfa</button></a>
+<span class="right">
+
+</span>
+<button class="btn"></button>
+<br><br>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<center>
+<div class="content">
+<img class="img" height="400" src="girl.png">
+<span class="p2">
+<b>&nbsp; 空の花 </b>
+<br>
+&nbsp;merhaba!<br>&nbsp;bana kısaca lorex diyebilirsin.<br>&nbsp;yazilim ile ilgileniyorum.<br><br>&nbsp;html , css , javascript <br>&nbsp;php ve python biliyorum.
+<br><br>
+&nbsp;<a target="_blank" href="https://discord.gg/QPWD63ZT4"><img height="40" src="https://cdn3.emoji.gg/emojis/8885-discord.png"></a>
+&nbsp;<a target="_blank" class="qq1" href="https://github.com/whoxami"><img height="40" src="https://cdn3.emoji.gg/emojis/6705-githubblack.png"></a>
+&nbsp;<a target="_blank" class="qq1" href="https://steamcommunity.com/id/whoxami/"><img height="40" src="https://cdn3.emoji.gg/emojis/5366-steam.png"></a>
+</span>
+</center>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<center><span class="supporterstext">Destekciler</span></center>
+<br><br><br><br><br><br><br><br>
+<center>
+<div class="supporters">
+<img class="img2" height="400" src="linux.png">
+<span class="p2">
+<b>&nbsp;ZoZo</b>
+<br>
+&nbsp;yazilim ile ilgilenmektedir.<br>&nbsp;html , css , python<br>&nbsp;c , c# ve cpp bilmektedir.<br>&nbsp;Linux hakkında bilgisi vardir.<br>&nbsp;ayrica kimya , elektrik bilgiside vardır.
+<br><a target="_blank" href="https://zozonunscatmekani.glitch.me">&nbsp;zozonunscatmekani.glitch.me</a><br><br>
+&nbsp;<a target="_blank" href="https://discord.gg/QPWD63ZT4"><img height="40" src="https://cdn3.emoji.gg/emojis/8885-discord.png"></a>
+</a>
+</span>
+</center>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<center>
+<div class="supporters">
+<img class="img2" height="400" src="linux.png">
+<span class="p2">
+<b>&nbsp;xnnx1337</b>
+<br>
+&nbsp;yazilim ile ilgilenmektedir.<br>&nbsp;html bilmektedir.<br>&nbsp;Linux hakkında bilgisi vardir.
+<br>
+&nbsp;<a target="_blank" href="https://discord.gg/QPWD63ZT4"><img height="40" src="https://cdn3.emoji.gg/emojis/8885-discord.png"></a>
+</a>
+</span>
+</center>
+</div>
+</center>
+<style>
+.qq1 {
+    margin:-20px;
+    padding:20px;
+}
+.img {
+    border-radius:15px;
+}
+.img2 {
+    border-radius:15px;
+}
+.supporterstext {
+    color:white;
+    font-size:25px;
+    font-family:arial;
+}
+.supporters {
+    text-align:left;
+    border:solid 3px black;
+    background-image:url(wp3.jpg);
+    border-radius:15px;
+    width:800px;
+}
+.content {
+    text-align:left;
+    border:solid 3px black;
+    background-image:url(wp3.jpg);
+    border-radius:15px;
+    width:800px;
+}
+.menu {
+    background-color:black;
+    color:black;
+}
+.right {
+    
+    position:absolute;
+    right:5%;
+}
+.p1 {
+    color:white;
+    font-size:25px;
+    font-family:monospace;
+}
+.p2 {
+    position:absolute;
+    color:white;
+    font-size:25px;
+    font-family:arial;
+}
+.btn {
+    text-decoration:underline;
+    font-size:23px;
+    background-color:transparent;
+    border:none;
+    color:white;
+    cursor:pointer;
+}
+a {
+    color:#007fff;
+}
+//* -------------------------------- *//
+* {
+        box-sizing: border-box;
+    }
+    body {
+        margin:0;
+        height: 100%;
+    }
+        .bg {
+            background-image: url();
+            background-size:cover;
+            background-repeat:no-repeat;
+            background-position:center center;
+ 
+            width: 100%;
+            height: 100%;
+        }
+</style>
+</body>
+</html>
